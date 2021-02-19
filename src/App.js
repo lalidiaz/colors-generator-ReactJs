0e1f4ee0904396bad5a3a168232fbfe2836e7824
Lali <lauradiaz1586@gmail.com>
2021-02-19 11:45:28 +0400

    add styles and state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,35 @@ import React, { useState } from 'react';
 import SingleColor from './SingleColor';
 
 function App() {
-  return <h2>app set up</h2>;
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#66bb6a"
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
 }
 
 export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+//convert rgb to hex
+
 function componentToHex(c) {
   var hex = c.toString(16);
   return hex.length == 1 ? '0' + hex : hex;
